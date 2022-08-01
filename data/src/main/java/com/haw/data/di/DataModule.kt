8fd0dcc389670886959f916fb9900ebc4103a6b1
HariAgus <haryagus97@gmail.com>
2022-08-01 09:32:40 +0700

    Setup DI module

---
+++ data/src/main/java/com/haw/data/di/DataModule.kt
@@ -0,0 +1,35 @@
+package com.haw.data.di
+
+import com.haw.data.api.CoinPaprikaApi
+import com.haw.data.repository.CoinRepositoryImpl
+import com.haw.domain.common.Constants.BASE_URL
+import com.haw.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+
+}
